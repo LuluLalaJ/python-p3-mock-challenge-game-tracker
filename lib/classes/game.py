7a71742c25a63010668b9562df81b81f4eeaec2f
Chen <chen.jiang.1206@gmail.com>
2023-05-09 12:13:06 -0400

    done -but please redo

--- lib/classes/game.py
+++ lib/classes/game.py
@@ -1,16 +1,34 @@
 class Game:
     def __init__(self, title):
-        self.title = title
+        if not (isinstance(title, str)) or len(title) <= 0:
+            raise Exception("Game title must be a string with length longer than 0")
+        self._title = title
         self._results = []
         self._players = []
-        
+
+    @property
+    def title(self):
+        return self._title
+
+    @title.setter
+    def title(self, val):
+        raise Exception("Game title cannot be changed after the Game is initialized.")
+
     def results(self, new_result=None):
         from classes.result import Result
-        pass
-    
+        if new_result and isinstance(new_result, Result):
+            self._results.append(new_result)
+        return self._results
+
     def players(self, new_player=None):
         from classes.player import Player
-        pass
-    
+        if new_player and isinstance(new_player, Player):
+            self._players.append(new_player)
+        return self._players
+
     def average_score(self, player):
-        pass
+        #zerodivision!!!
+        scores = [result.score for result in self._results if result.player == player]
+        if scores:
+            return sum(scores)/len(scores)
+        return 0
--- lib/classes/player.py
+++ lib/classes/player.py
@@ -3,25 +3,50 @@ class Player:
     all = []
 
     def __init__(self, username):
-        self.username = username
+        if not isinstance(username, str) or not (2 <= len(username) <= 16):
+            raise Exception("Username must be a string and between 2 and 16 characters")
+        self._username = username
         self._results = []
         self._games_played = []
-        
+        self.all.append(self)
+
+    @property
+    def username(self):
+        return self._username
+
+    @username.setter
+    def username(self, val):
+        if isinstance(val, str) and (2 <= len(val) <= 16):
+            self._username = val
+        else:
+            raise Exception("Username must be a string and between 2 and 16 characters")
+
     def results(self, new_result=None):
         from classes.result import Result
-        pass
-    
+        if new_result and isinstance(new_result, Result):
+            self._results.append(new_result)
+        return self._results
+
     def games_played(self, new_game=None):
         from classes.game import Game
-        pass
-    
+        if new_game and isinstance(new_game, Game):
+            self._games_played.append(new_game)
+        return self._games_played
+
     def played_game(self, game):
-        pass
-    
+        return self in game.players()
+
     def num_times_played(self, game):
-        pass
-    
+        return len([result for result in self._results if result.game == game])
+
     @classmethod
     def highest_scored(cls, game):
-        pass
-        
+        if cls.all:
+            maxscore = 0
+            maxplayer = None
+            for player in cls.all:
+                if game.average_score(player) > maxscore:
+                    maxscore = game.average_score(player)
+                    maxplayer = player
+            return maxplayer
+        return None 
